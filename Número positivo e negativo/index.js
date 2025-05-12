b9ad3ddaf852b805bc76f1c3ae59e72e91639371
Maciel-NJ <asbahr.samuel10@outlook.com>
2025-05-12 14:52:02 -0300

    Update index.js

--- Número positivo e negativo/index.js
+++ Número positivo e negativo/index.js
@@ -1,7 +1,7 @@
-let numero = parseFloat(prompt("Digite um número positivo:"));
+let numero = parseFloat(prompt("Digite um número positivo:")); // serve para coletar o número
 
 while (numero < 0 || isNaN(numero)) {
-  numero = parseFloat(prompt("Valor inválido! Digite um número **positivo**:"));
+  numero = parseFloat(prompt("Valor inválido! Digite um número **positivo**:")); // identifica se o número é positivo ou negativo se caso for negativo o usuario será notificado
 }
 
-alert("Número válido digitado: " + numero);
+alert("Número válido digitado: " + numero);  // informa que o número digitado é correto
