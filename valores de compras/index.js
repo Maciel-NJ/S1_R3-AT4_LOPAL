b8f031bece5dbcd6900fddf74396fa0635373e60
Maciel-NJ <asbahr.samuel10@outlook.com>
2025-05-12 14:47:34 -0300

    Update index.js

--- valores de compras/index.js
+++ valores de compras/index.js
@@ -1,18 +1,18 @@
 
-let valor = parseFloat(prompt("Digite o valor da compra (ou 0 para finalizar):"));  // Pede o primeiro valor da compra
+let valor = parseFloat(prompt("Digite o valor da compra (ou 0 para finalizar):"));  // pede o 1° valor de compra
 let total = 0;
 
 
-while (valor !== 0) {  // Enquanto o valor for diferente de 0, continua somando
+while (valor !== 0) {  // enquanto o valor for diferente de 0 ele continuará somando
   if (valor > 0) {  
-    total += valor; // Soma ao total se for um valor positivo
+    total += valor; // se for um valor positivo somará ao total
   } else {
     alert("Valor inválido! Digite um número positivo ou 0 para finalizar.");
   }
 
   
-  valor = parseFloat(prompt("Digite o valor da compra (ou 0 para finalizar):"));  // Pede o próximo valor
+  valor = parseFloat(prompt("Digite o valor da compra (ou 0 para finalizar):"));  // pede o próximo valor
 }
 
 
-alert("Total da compra: R$ " + total.toFixed(2));  // Exibe o total da compra
+alert("Total da compra: R$ " + total.toFixed(2));  // mostra o total da compra
