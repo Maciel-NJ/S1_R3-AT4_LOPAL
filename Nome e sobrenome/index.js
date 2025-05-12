0523a7bbd77ef6fb225a850bcfe0f57361f7d86a
Maciel-NJ <asbahr.samuel10@outlook.com>
2025-05-12 14:53:54 -0300

    Update index.js

--- Nome e sobrenome/index.js
+++ Nome e sobrenome/index.js
@@ -1,17 +1,17 @@
-let primeiroNome = prompt("Digite seu primeiro nome:");
+let primeiroNome = prompt("Digite seu primeiro nome:");  //coleta o 1° nome
 
 
-while (primeiroNome === "") {  // Enquanto o primeiro nome for uma string vazia, continua pedindo
+while (primeiroNome === "") {  // se o primeiro nome for uma string vazia continuará pedindo
   primeiroNome = prompt("Primeiro nome não pode ficar em branco. Digite seu primeiro nome:");
 }
 
 
-let sobrenome = prompt("Digite seu sobrenome:");  // Pede o sobrenome
+let sobrenome = prompt("Digite seu sobrenome:");  // pede o sobrenome
 
 
-while (sobrenome === "") {   // Enquanto o sobrenome for uma string vazia, continua pedindo
+while (sobrenome === "") {   // se o sobrenome for uma string vazia continuará pedindo
   sobrenome = prompt("Sobrenome não pode ficar em branco. Digite seu sobrenome:");
 }
 
 
-alert("Nome completo: " + primeiroNome + " " + sobrenome);   // Exibe o nome completo
+alert("Nome completo: " + primeiroNome + " " + sobrenome);   // mostra o nome completo
